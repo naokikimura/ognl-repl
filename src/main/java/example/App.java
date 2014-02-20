445fb22be5b6aa953b05eea1064a77435de68b97
naokikimura <n.kimura.cap@gmail.com>
2014-02-21 03:04:09 +0900

    Adds source files

---
+++ src/main/java/example/App.java
@@ -0,0 +1,87 @@
+package example;
+
+import java.io.BufferedReader;
+import java.io.Console;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.util.Map;
+import java.util.ResourceBundle;
+import ognl.Ognl;
+import ognl.OgnlContext;
+import ognl.OgnlException;
+
+/**
+ *
+ * @author <a href="https://github.com/naokikimura">naokikimura</a>
+ *
+ */
+public abstract class App {
+
+    private static final ResourceBundle resources = ResourceBundle.getBundle(App.class.getName());
+
+    public abstract void execute(final Map context, final Object root) throws Exception;
+
+    static App create(final InputStream in, final PrintStream out, final PrintStream err) {
+        return new App() {
+            @Override
+            public void execute(final Map context, final Object root) throws Exception {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+                String expression;
+                while ((expression = reader.readLine()) != null) {
+                    try {
+                        Object value = Ognl.getValue(expression, context, root);
+                        out.printf(resources.getString("out.printf.format"), value);
+                    } catch (OgnlException e) {
+                        err.printf(resources.getString("err.printf.format"), e);
+                    }
+                }
+            }
+        };
+    }
+
+    static App create(final Console console, final PrintWriter out, final PrintStream err) {
+        return new App() {
+            @Override
+            public void execute(final Map context, final Object root) throws Exception {
+                int line = 0;
+                while (true) {
+                    String expression = console.readLine(resources.getString("console.readLine.format"), ++line);
+                    if (expression == null) {
+                        console.printf("%n");
+                        return;
+                    } else if (expression.trim().equals("")) {
+                        continue;
+                    }
+
+                    try {
+                        Object value = Ognl.getValue(expression, context, root);
+                        out.printf(resources.getString("console.printf.format"), value);
+                    } catch (OgnlException e) {
+                        err.printf(resources.getString("err.printf.format"), e);
+                    }
+                }
+            }
+        };
+    }
+
+    static App create() {
+        Console console = System.console();
+        if (console == null) {
+            return App.create(System.in, System.out, System.err);
+        } else {
+            return App.create(console, console.writer(), System.err);
+        }
+    }
+
+    static Object newRootInstance() throws IllegalAccessException, InstantiationException, ClassNotFoundException {
+        String rootClassName = System.getProperty("ognl.repl.root");
+        return rootClassName == null ? null : Class.forName(rootClassName).newInstance();
+    }
+
+    public static void main(String[] args) throws Exception {
+        App app = create();
+        app.execute(new OgnlContext(), newRootInstance());
+    }
+}
---
+++ src/test/java/example/AppTest.java
@@ -0,0 +1,18 @@
+package example;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigourous Test :-)
+     */
+    @Test
+    public void testApp() {
+        assertTrue(true);
+    }
+}
